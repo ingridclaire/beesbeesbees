a3b205a2b63b6eb026bb0913c68f6f62db845e5a
Ingrid Carlson <ingridclaire@gmail.com>
2020-03-30 13:00:57 -0700

    HoneyMakerBee class!

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,24 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0
 };
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function () {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function () {
+  this.honeyPot--;
+}
+
+/*
+a makeHoney method that adds 1 to that honeyBee\'s honeyPot
+ a giveHoney method that subtracts 1 from that honeyBee\'s honeyPot
+ */
+
 
